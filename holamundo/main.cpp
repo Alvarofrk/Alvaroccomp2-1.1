1f8acd6edd1e7d502bc51da741d3a209a8082895
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-03-21 08:53:07 -0500

    Update main.cpp

--- holamundo/main.cpp
+++ holamundo/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "hola mundo" << endl;
+    int x=5;
+    int y=6;
+    int z=x+y;
+    cout<<"el primer numero: "<<x<<endl;
+    cout<<"el segundo numero: "<<y<<endl;
+    cout<<"la respuesta es :"<<z<<endl;
     return 0;
 }
